d1afd3f53379f42585742a8bf43948063321e67c
mrjoshdev <wyman.josh@gmail.com>
2018-11-07 16:23:15 -0500

    Added validations to the models and views for the three letter ticker symbol

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -1,6 +1,7 @@
 class Cryptocurrency < ApplicationRecord
   validates :name, presence: true
-  validates :symbol, presence: true
+  validates :symbol, presence: true, length: { is: 3 }, format: { with: /\A[A-Z]+\Z/,
+    message: "only allows capital letters" }
   validates :target_price, presence: true
 
   belongs_to :user
